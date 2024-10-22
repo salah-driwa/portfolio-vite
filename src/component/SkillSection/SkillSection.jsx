ea4d6ea9ddc2ad5a4a8dd5d14e8c2b48983829ed
salah-driwa <hamdiessdriwa121@gmail.com>
2024-10-22 02:35:49 +0100

    10/22

--- src/component/SkillSection/SkillSection.jsx
+++ src/component/SkillSection/SkillSection.jsx
@@ -7,7 +7,7 @@ const SkillSection = () => {
     return ( 
     <Section>
         <div className='xl:mx-80 lg:mx-20  mx-7' id='skills'> 
-            <h1 className='text-primary  font-mono  font-bold 
+            <h1 className='text-primary    font-bold 
              text-2xl   sm:text-4xl   sm:m-11   text-center
               '>Skills And Framework ..</h1>
 
@@ -15,8 +15,8 @@ const SkillSection = () => {
                 <Section delay={0.1} x={20} duration={0.3} >
                     <div className='flex sm:flex-row  flex-col  justify-evenly '>
                         <div className='  w-full'>
-                            <h1 className=" text-left text-secondary font-bold font-mono text-3xl  sm:pt-10">Adobe Suite</h1>
-                            <p  className=' text-left text-secondary font-normal font-mono text-base leading-relaxed   pt-4  sm:text-xl'>I am highly proficient in using the Adobe Creative Suite, including popular software such as Illustrator, Photoshop, and InDesign.</p>
+                            <h1 className=" text-left text-secondary font-bold  text-3xl  sm:pt-10">Adobe Suite</h1>
+                            <p  className=' text-left text-secondary font-normal  text-base leading-relaxed   pt-4  sm:text-xl'>I am highly proficient in using the Adobe Creative Suite, including popular software such as Illustrator, Photoshop, and InDesign.</p>
                         </div>    
                    <Rive  stateMachine={'State Machine 1'} artboard={'adobe'} height={'300px'} width={''} />
                    </div>
@@ -25,13 +25,13 @@ const SkillSection = () => {
                  <Section delay={0.2} x={200} duration={0.9}>
                  <div className='flex  sm:flex-row  flex-col  justify-evenly '>
                     <div className='sm:hidden block'>
-                 <h1 className=" text-left text-secondary font-bold font-mono text-3xl uppercase sm:pt-10 ">frontend</h1>
-                <p  className=' text-left text-secondary font-normal font-mono text-base leading-relaxed    p-3  sm:text-xl'>I enjoy working with front-end technologies, especially React, Next.js,framer motion Tailwind CSS, and Bootstrap.</p>
+                 <h1 className=" text-left text-secondary font-bold  text-3xl uppercase sm:pt-10 ">frontend</h1>
+                <p  className=' text-left text-secondary font-normal  text-base leading-relaxed    p-3  sm:text-xl'>I enjoy working with front-end technologies, especially React, Next.js,framer motion Tailwind CSS, and Bootstrap.</p>
                 </div>
                  <Rive  stateMachine={'State Machine 1'} artboard={'frontend'} height={'300px'} />
                  <div className='hidden sm:block'>
-                            <h1 className=" text-left text-secondary font-bold font-mono text-3xl uppercase sm:pt-10 ">frontend</h1>
-                            <p  className=' text-left text-secondary font-normal font-mono text-base leading-relaxed    pt-3  sm:text-xl'>I enjoy working with front-end technologies, especially React, Next.js,framer motion Tailwind CSS, and Bootstrap.</p>
+                            <h1 className=" text-left text-secondary font-bold - text-3xl uppercase sm:pt-10 ">frontend</h1>
+                            <p  className=' text-left text-secondary font-normal - text-base leading-relaxed    pt-3  sm:text-xl'>I enjoy working with front-end technologies, especially React, Next.js,framer motion Tailwind CSS, and Bootstrap.</p>
                         </div>  
                     
                     </div>
@@ -40,8 +40,8 @@ const SkillSection = () => {
                  <Section delay={0.2} x={-120} duration={0.3}> 
                  <div className='flex sm:flex-row  flex-col   justify-evenly  mt-12'>
                  <div className=''>
-                            <h1 className=" text-left text-secondary font-bold font-mono text-3xl uppercase sm:pt-10">backend</h1>
-                            <p  className=' text-left text-secondary font-normal font-mono text-base leading-relaxed    pt-3  sm:text-xl'>  
+                            <h1 className=" text-left text-secondary font-bold  text-3xl uppercase sm:pt-10">backend</h1>
+                            <p  className=' text-left text-secondary font-normal  text-base leading-relaxed    pt-3  sm:text-xl'>  
                              I have experience in backend development using technologies such
                               as MongoDB, PHP, MySQL, and API integrations such as sanity
                             </p>
