9f077b6fd1dbd0f977c0d3bfc7170b9cc4053243
salah-driwa <hamdiessdriwa121@gmail.com>
2024-12-30 12:34:34 +0100

    added images from arcane study

--- src/asets/Projects/digital  Illustration/digitalIllustrationProjects.js
+++ src/asets/Projects/digital  Illustration/digitalIllustrationProjects.js
@@ -6,6 +6,22 @@ import desertV2 from './images/true-dark-dessert-v2.jpg';
 import desert from './images/true-dark-desert.jpg';
 import godOfWarFanart from './images/true-dark-layer-108.jpg';
 import rakenAndBardFanArt from './images/true-dark-final2k.jpg';
+import arcane1 from './images/arcane/Layer 84.jpg';
+import arcane2 from './images/arcane/Layer 85.jpg';
+import arcane3 from './images/arcane/Layer 86.jpg';
+import arcane4 from './images/arcane/Layer 87.jpg';
+import arcane5 from './images/arcane/Layer 88.jpg';
+import arcane6 from './images/arcane/Layer 89.jpg';
+import arcane7 from './images/arcane/Layer 90.jpg';
+import arcane8 from './images/arcane/Layer 91.jpg';
+import arcane9 from './images/arcane/Layer 91.jpg';
+import arcane10 from './images/arcane/Layer 62layer.jpg';
+import arcane11 from './images/arcane/Layer 63layer.jpg';
+import arcane12 from './images/arcane/Layer 64layer.jpg';
+import arcane13 from './images/arcane/Layer 65layer.jpg';
+import arcane14 from './images/arcane/Layer 66layer.jpg';
+import arcane15 from './images/arcane/Layer 67layer.jpg';
+import arcane16 from './images/arcane/Layer 68layer.jpg';
 
 
 const digitalIllustrationProjects = [
@@ -28,6 +44,66 @@ const digitalIllustrationProjects = [
     link: "https://www.artstation.com/artwork/ArYazm",
     icons: [4, 1],
     icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png"
+  },
+  {
+    img: godOfWarFanart,
+    title: 'God of War Fanart',
+    description: 'Fan art inspired by God of War.',
+    tags: ['#splash art'],
+    url: "",
+    link: "https://www.artstation.com/artwork/B3D3J4",
+    icons: [4],
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png"
+  },
+  {
+    img: [arcane1, arcane2, arcane3],
+    title: 'Arcane Study - Part 1',
+    description: 'A detailed study of Arcane characters and scenes. Part 1.',
+    tags: ['#Arcane #Study'],
+    url: "",
+    link: "https://www.artstation.com/artwork/lGNPQG",
+    icons: [4],
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
+  },
+  {
+    img: [arcane4, arcane5, arcane6],
+    title: 'Arcane Study - Part 2',
+    description: 'Exploring the vibrant visuals of Arcane. Part 2.',
+    tags: ['#Arcane #Study'],
+    url: "",
+    link: "https://www.artstation.com/artwork/P6yw0y",
+    icons: [4],
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
+  },
+  {
+    img: [arcane7, arcane8, arcane9],
+    title: 'Arcane Study - Part 3',
+    description: 'In-depth artistic exploration of Arcane. Part 3.',
+    tags: ['#Arcane #Study'],
+    url: "",
+    link: "https://www.artstation.com/artwork/wrZWG9",
+    icons: [4],
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
+  },
+  {
+    img: [arcane10, arcane11, arcane12],
+    title: 'Arcane Study - Part 4',
+    description: 'Analyzing Arcane’s unique visual style. Part 4.',
+    tags: ['#Arcane #Study'],
+    url: "",
+    link: "https://www.artstation.com/artwork/ZlRvY1",
+    icons: [4],
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
+  },
+  {
+    img: [arcane13, arcane14, arcane15,arcane16],
+    title: 'Arcane Study - Part 5',
+    description: 'Final set of illustrations inspired by Arcane. Part 5.',
+    tags: ['#Arcane #Study'],
+    url: "",
+    link: "https://www.artstation.com/artwork/Ova50e",
+    icons: [4],
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png",
   },
   {
     img: trueDarkFinalCitySwitch,
@@ -58,16 +134,7 @@ const digitalIllustrationProjects = [
     icons: [3],
     icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrfuq_ek1dz9hu5STinduyTCr9holwrdrQ2w&s"
   },
-  {
-    img: godOfWarFanart,
-    title: 'God of War Fanart',
-    description: 'Fan art inspired by God of War.',
-    tags: ['#splash art'],
-    url: "",
-    link: "https://www.artstation.com/artwork/B3D3J4",
-    icons: [4],
-    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/2101px-Adobe_Photoshop_CC_icon.svg.png"
-  },
+
   {
     img: rakenAndBardFanArt,
     title: 'Raken and Bard Fan Art League of Legends',
