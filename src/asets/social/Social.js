cae886cc93e23b7c5db270589d474394cb2b14d0
salah-driwa <hamdiessdriwa121@gmail.com>
2025-02-09 17:00:37 +0100

    adding linkdin

--- src/asets/social/Social.js
+++ src/asets/social/Social.js
@@ -1,6 +1,6 @@
 
 import { FiGithub } from 'react-icons/fi';
-import { FaArtstation } from 'react-icons/fa';
+import { FaArtstation, FaLinkedinIn } from 'react-icons/fa';
 import { RiInstagramLine } from 'react-icons/ri';
 import { LuFacebook } from 'react-icons/lu';
 import { AiOutlineYoutube } from 'react-icons/ai';
@@ -12,9 +12,10 @@ const Social = [
     { icon: RiInstagramLine, url: 'https://www.instagram.com/truedark.art/' },
     { icon: LuFacebook, url: 'https://www.facebook.com/TrueDark.art1' },
     { icon: AiOutlineYoutube, url: 'https://www.youtube.com/channel/UC7kJKlt8WyzAjnDAEYSc1Og/featured' },
+    { icon: FaLinkedinIn, url: 'https://www.linkedin.com/in/salah-driwa-73b28a255/' },
   ];
   
- 
+
 export default Social;
 
 
